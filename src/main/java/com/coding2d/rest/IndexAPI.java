71feef2ed88c991b135ec73c7620a320c91ae6a7
gergely <kiss-gergely@outlook.com>
2018-11-26 15:01:54 +0000

    update small stuff

--- src/main/java/com/coding2d/rest/IndexAPI.java
+++ src/main/java/com/coding2d/rest/IndexAPI.java
@@ -1,15 +1,20 @@
 package com.coding2d.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexAPI {
+
+	private static final Logger LOG = LoggerFactory.getLogger(IndexAPI.class); 
 	
 	@RequestMapping(value = "/")
 	@GetMapping
-	public String index() {
+	public String getIndex() {
+		LOG.info("getIndex was called");
 		return "index.html";
 	}
 }
